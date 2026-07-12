51343bd8b9ad07265428667dc8591b1e1c6b141c
Dhruvil Mistry <dhruvilmistry16@gmail.com>
2026-07-12 23:47:06 +0530

    chore: commit index.ts (components/landing/index.ts)

---
+++ components/landing/index.ts
@@ -0,0 +1,8 @@
+export { default as ProblemSection } from "./problem-section";
+export { default as HowItWorks } from "./how-it-works";
+export { default as FeaturesBento } from "./features-bento";
+export { default as ProductShowcase } from "./product-showcase";
+export { default as UseCases } from "./use-cases";
+export { default as Integrations } from "./integrations";
+export { default as FAQSection } from "./faq-section";
+export { default as FinalCTA } from "./final-cta";
